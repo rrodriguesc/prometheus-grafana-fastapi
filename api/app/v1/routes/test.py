446b1813430dfd7b6813016de7257d0504e36ce6
rafaelrcarvalho <rrodriguesdecarvalho@gmail.com>
2022-02-02 14:18:38 -0300

    Modify status code

--- api/app/v1/routes/test.py
+++ api/app/v1/routes/test.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter, Body, Query, Request, Response
+from typing import Dict
+
+from fastapi import APIRouter, Body, Query, Request, Response, status
 
 from app.v1.models import Message, Result, Status
 
 router = APIRouter()
+
+STATUS_MAP: Dict[Status, int] = {
+    Status.OK: status.HTTP_200_OK,
+    Status.ERROR: status.HTTP_500_INTERNAL_SERVER_ERROR,
+    Status.FAIL: status.HTTP_400_BAD_REQUEST,
+}
 
 
 @router.get("/check", response_model=Result, tags=["Test"])
@@ -12,9 +20,10 @@ async def check(*, request: Request, response: Response) -> Result:
 
 @router.get("/check-status", response_model=Result, tags=["Test"])
 async def check_status(
-    *, status: Status = Query(Status.OK), request: Request, response: Response
+    *, status_info: Status = Query(Status.OK), request: Request, response: Response
 ) -> Result:
-    return Result(status=status)
+    response.status_code = STATUS_MAP[status_info]
+    return Result(status=status_info)
 
 
 @router.post("/message", response_model=Message, tags=["Test"])
